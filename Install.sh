af2c39533d4db47044df45894bb5955809592065
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-01-28 17:01:09 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#
+#Yay if not installed
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# Lightdm
 sh sysctl.sh
-#
+
+#Lightdm Theme
 sh Lightdm Aether.sh
-#
+
+#Ranger Devicons
 sh Ranger-Devicons.sh
-#
+
+# .config
 sh CP.sh
-#
+
+#Amongus
