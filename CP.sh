a9040099e2e5de4270fdd08d48616c596e02f111
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-01-29 01:15:41 -0300

    Update CP.sh

--- CP.sh
+++ CP.sh
@@ -6,5 +6,10 @@ cp -r alacritty/ ~/.config
 #neofetch configuration
 cp -r neofetch/ ~/.config  
  
- #qtile Configurations
- cp -r qtile/ ~/.config  
+#qtile Configurations
+cp -r qtile/ ~/.config 
+ 
+#Wallpaper
+mkdir $HOME .Wallpaper
+
+cp -r Arch-Linux-Wallpaper-4K.png ~/.Wallpapers
