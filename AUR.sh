22bb6ef879899a3668b7740b14af9f63d73a4f82
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-01-28 22:40:34 -0300

    Update AUR.sh

--- AUR.sh
+++ AUR.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 #Yay if not installed
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
 #google Chrome
 yay google-chrome
+
+#Nerd Font
+yay -S nerd-fonts-ubuntu-mono
