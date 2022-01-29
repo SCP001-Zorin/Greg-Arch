31bea5229086656e6fe3f04dfb5ee46675c20dda
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-01-28 21:56:11 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+sudo pacman -Sy
 #change languege language to US AMERICA
 sudo locale-gen us
-#Yay if not installed
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
 # Lightdm
 sh sysctl.sh
 
