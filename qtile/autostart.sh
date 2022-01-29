51f4413f6d34b9debbbe38fc82505545a13d771a
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-01-29 01:50:27 -0300

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+#Launch picom
+picom -b & 
+
 # systray battery icon
 cbatticon -u 5 &
 
 # systray volume
 volumeicon &
-
-#Launch picom
-picom -b & 
 
 # Feh
 feh --bg-scale /home/krennic/.Wallpapers/Arch-Linux-Wallpaper-4K.png
