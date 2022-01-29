973b0b461d0327d9a20a01c887bde76cd129b037
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-01-28 22:55:03 -0300

    Update yay-install.sh

--- yay-install.sh
+++ yay-install.sh
@@ -2,9 +2,3 @@
 
 #Yay if not installed
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-
-#google Chrome
-yay google-chrome
-
-#Nerd Font
-yay -S nerd-fonts-ubuntu-mono
