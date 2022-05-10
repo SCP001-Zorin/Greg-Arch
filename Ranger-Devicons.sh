c9a52e533c249485ef53df588236bf84a8795464
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-05-10 13:41:17 -0500

    Delete Ranger-Devicons.sh

--- Ranger-Devicons.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Devicons plugin that add icons to ranger file manager
-git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-#Set new configuration as default
-echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
